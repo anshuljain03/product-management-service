c0955a0777f94b2118dcb6d5dd717b1f79bd5a9a
Anshul Jain <anshuljain96.aj@gmail.com>
2023-01-27 01:58:55 +0530

    Added more details in coupon model

--- src/models/coupon.model.js
+++ src/models/coupon.model.js
@@ -6,6 +6,13 @@ module.exports = (sequelize) => {
 			type: DataTypes.STRING,
 			primaryKey: true
 		},
+		// Percentage or flat discount
+		coupon_type: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		// This will be a JSON object with the following structure: { "value":"20", "min_amount": 100, "max_discount": 200 }.
+		// This has been kept as a JSON object so that it is extensible. Ideally this would be in a NoSQL database.
 		details: {
 			type: DataTypes.JSON,
 			allowNull: false
